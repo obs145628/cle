9871859fed0dd1d91710b5ff7f5565f1c82343e2
Steven Lariau <obs145628@gmail.com>
2020-08-13 22:27:06 +0200

    update launch tests

--- check.sh
+++ check.sh
@@ -2,14 +2,12 @@
 
 # Basic script to run build and test command on all projects
 
-# Use `git clean -dfx` to make sure repo is clean befure runing tests
+# Use `git clean -dfx` to make sure repo is clean before runing tests
 
 CMAKE_CMD="make -j8 && make check -j8"
 if [[ $* == *--only-build* ]]; then {
   CMAKE_CMD="make -j8"
 } fi
-
-
 
 check_cmake_proj() {
     (
@@ -27,7 +25,28 @@ check_cmake_proj() {
     } fi
 }
 
+CARGO_CMD="cargo build && cargo test"
+if [[ $* == *--only-build* ]]; then {
+  CARGO_CMD="cargo build"
+} fi
+
+
+
+check_cargo_proj() {
+    (
+	echo "Testing $1 ..."
+	cd ./$1;
+	eval $CARGO_CMD
+
+    )
+    if [ $? -ne 0 ]; then {
+	echo "Tests failed for project $1";
+	exit 1
+    } fi
+}
+
 check_cmake_proj utils/libcpp_utils
+check_cargo_proj utils/mdlogger/server
 check_cmake_proj utils/mdlogger/libcpp
 check_cmake_proj utils/libcpp_gop10
 
@@ -35,3 +54,4 @@ check_cmake_proj backend/inst-sched/local-list
 check_cmake_proj backend/inst-sched/local-list-eb
 check_cmake_proj backend/reg-alloc/block-naive
 check_cmake_proj backend/reg-alloc/block-bottomup
+check_cmake_proj backend/reg-alloc/color-ssa-td
